dcc6d831df8d1850065c04d7a48b1695cb276c10
Rogue-Telvanni <joaocopini11@outlook.com>
2025-11-16 14:55:29 -0300

    ajuste do git ignote para remover a gramatica compilada. adição do exemplo de erro

---
+++ Trabalho_Grau_B/exemplo-erro.js
@@ -0,0 +1,13 @@
+let teste = "string de teste
+
+
+
+function functest(){
+    return 2 + 2;
+
+
+
+function loadReferenceTable() {
+    const tbody = document.getElementById('tableBody');
+    tbody.innerHTML = referenceData.map((item, index) = 'teste').join('');
+}
